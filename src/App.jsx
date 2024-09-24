045c9be37fd3463f4117427c7c858df32e152796
sathu341 <sthishchandran321@gmail.com>
2024-09-24 12:27:38 +0530

    thrid commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 
 import './App.css'
 import Board from './board'
-import Aboutpage from './components/aboutpage'
-import Cardskerala from './components/cardskerala'
-import Headernav from './components/headernav'
-import Homepage from './components/homepage'
-import Slider from './components/slider'
-import {Routes,Route} from 'react-router-dom';
-import { lazy,Suspense } from 'react'
-import Spinner from 'react-bootstrap/Spinner';
+// import Aboutpage from './components/aboutpage'
+// import Cardskerala from './components/cardskerala'
+// import Headernav from './components/headernav'
+// import Homepage from './components/homepage'
+// import Slider from './components/slider'
+// import {Routes,Route} from 'react-router-dom';
+// import { lazy,Suspense } from 'react'
+// import Spinner from 'react-bootstrap/Spinner';
 function App() {
-const Hpage=lazy(()=> import('./components/homepage'))
-const AboutPage=lazy(()=> import('./components/aboutpage'))
+// const Hpage=lazy(()=> import('./components/homepage'))
+// const AboutPage=lazy(()=> import('./components/aboutpage'))
   return (
     <>
-      <Headernav/>
+      {/* <Headernav/>
       <section>
         <Suspense fallback={<div>
           <Spinner animation="border" role="status">
@@ -29,8 +29,8 @@ const AboutPage=lazy(()=> import('./components/aboutpage'))
         </Suspense>
   
 
-      </section>
-   
+      </section> */}
+   <Board/>
 
     </>
   )
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
+import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//   },
+// });
+export default defineConfig( {
   build: {
-    outDir: 'dist',
-  },
-});
+    rollupOptions: {
+      external: ['some-external-package']
+    }
+  }
+})
